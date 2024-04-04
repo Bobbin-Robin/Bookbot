6a4773bf1c5f77d4c4966d57302924cd8ccb61da
Bobbin-Robin <robbiehing@gmail.com>
2024-04-04 14:23:57 +0100

    now prints word count

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def main():
+    word_list = []
+    word_count = 0
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+        word_list = file_contents.split()
+        for i in word_list:
+            word_count += 1
+    print(word_count)
+     
+        
+
 main()

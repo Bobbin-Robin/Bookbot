a0a8067358bd6202e8dd5e6ddf868f017e371a4f
Bobbin-Robin <robbiehing@gmail.com>
2024-04-05 13:01:37 +0100

    added character counter

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 def main():
     word_list = []
     word_count = 0
+    from collections import defaultdict
+    character_dict = defaultdict(int)
     with open("./books/frankenstein.txt") as f:
         file_contents = f.read()
         word_list = file_contents.split()
-        for i in word_list:
+        for word in word_list:
             word_count += 1
+            word_lower = word.lower()
+            for char in word_lower:
+                character_dict[char] += 1    
+                
+    pass
     print(word_count)
-     
-        
+
+    pass
+    print(file_contents)   
+
+    
+    print(character_dict)
+
+
 
 main()
